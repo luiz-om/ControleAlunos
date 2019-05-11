8acdca1514c2a010ae6487ce2c0bc95c45392d32
Luiz-OM <luizfernando_martins@hotmail.com>
2019-05-11 12:38:02 -0400

    criação do projeto

---
+++ Context/SistemaContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ControleAulnos.Context
+{
+    public class SistemaContext : DbContext
+    {
+            
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ControleAulnos.Startup))]
+namespace ControleAulnos
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
